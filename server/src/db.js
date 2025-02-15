c4d541b93ba4dd0b59582950fba0052807e38ed9
angiecdev <angie@sweif.com>
2025-02-15 16:37:22 -0500

    Configuración de las variables de entorno finalizadas

--- server/src/db.js
+++ server/src/db.js
@@ -1,10 +1,18 @@
 import pg from 'pg';
+import { 
+    DB_USER, 
+    DB_HOST, 
+    DB_PASSWORD, 
+    DB_DATABASE, 
+    DB_PORT 
+} from './config.js';
+
 
 export const pool = new pg.Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'admin1234',
-    database: 'nodeexpress',
-    port: '5432'
+    user: DB_USER,
+    host: DB_HOST,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    port: DB_PORT,
 });
 
--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,17 @@ import { PORT } from "./config.js";
 import usersRoutes from './routes/users.routes.js';
 import morgan from 'morgan';
 
+
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(usersRoutes);
 
-app.listen(PORT)
-console.log('Server running on port', PORT);
+const port = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 
